efd26e361a0ed56f12d0bec4ea92baedd1415c5d
iñaki <debau123@gmail.com>
2024-12-09 15:41:21 +0000

    prueba gerarf

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+export default function dashboard(){
+
+    return(
+        <>
+          <h1>Este seria el invoices</h1>
+        </>
+    );
+}
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+export default function dashboard(){
+
+    return(
+        <>
+          <h1>Este seria el dashboard</h1>
+        </>
+    );
+}
